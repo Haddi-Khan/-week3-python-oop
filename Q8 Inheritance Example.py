a0a4438e64f7347e19f702a068d6af95cccc129d
haddi khan <haddik011@gmail.com>
2025-08-28 12:26:32 +0500

    Add files via upload

---
+++ Q8 Inheritance Example.py
@@ -0,0 +1,20 @@
+# 8.	Inheritance Example – Class Person → subclass Student.
+class person:
+    def __init__(self,name,age):
+        self.name= name
+        self.age = age 
+    def display_detail(self):
+        print(f"The name of a peron is {self.name} and his age is {self.age}")
+class student(person):
+    def __init__(self, name, age,student_id):
+        super().__init__(name, age)
+        self.student_id = student_id
+    def display(self):
+        print(f"Name: {self.name}, Age: {self.age}, Student ID: {self.student_id}")    
+        
+        
+        
+a=person("haddi",23)
+a.display_detail() 
+s = student("Haddi", 23, "S101")
+s.display()
