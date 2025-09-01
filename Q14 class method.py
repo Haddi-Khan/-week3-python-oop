9eeaae587810170ccf7cd8a6561e6aa461ef7b83
haddi khan <haddik011@gmail.com>
2025-09-01 10:30:13 +0500

    Add files via upload

---
+++ Q14 class method.py
@@ -0,0 +1,14 @@
+# 14.	Class Method Example – Method to count number of objects created.
+class Student():
+    count = 0
+    def __init__(self,name):
+        self.name=name
+        Student.count +=1
+    @classmethod
+    def total_students(cls):
+        return cls.count
+s1 = Student("Haddi")
+s2 = Student("Ali")
+s3 = Student("Zara")
+print("Total Students:", Student.total_students())
+    
